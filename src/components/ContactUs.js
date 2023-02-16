0eeda5b3d00ba441b060a148b6c4dafd4ef93906
unknown <baltaguirre@gmail.com>
2023-02-16 02:23:58 -0300

    first commit, header background images styles added

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -5,10 +5,22 @@ export default class ContactUs extends Component {
     return (
       <section id="contact">
           <div className="row section-head">
-            <div className="ten columns">
+            {/* <div className="ten columns">
               <p className="lead">
               Feel free to contact me for any work or suggestions below
               </p>
+            </div> */}
+             <div className="row">
+              <div className="columns contact-details">
+                <h2>Contact Details</h2>
+                <p className="address">
+                  <span>{resumeData.name}</span>
+                  <br />
+                  <span>{resumeData.address}</span>
+                  <br />
+                  <span>{resumeData.website}</span>
+                </p>
+              </div>
             </div>
           </div>
           <div className="row">
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,170 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Baltasar Aguirre",
+  role: "Full-Stack Web Developer",
+  linkedinId: "Your LinkedIn Id",
+  skypeid: "Your skypeid",
+  roleDescription: "Full-Stack Web & Mobile Developer",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/baltasar-aguirre-fullstack/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/Baltaguirre",
+      className: "fa fa-github",
+    },
+    {
+      name: "skype",
+      url: "http://twitter.com/rbhatia46",
+      className: "fa fa-twitter",
+    },
+  ],
+  aboutme: [
+    "▲ Problem solver, proactive, go-getter.",
+    "▲ Professional, empathic, team player.",
+    "▲ 2+ years of professional experience.",
+    "▲ Mid-Level experience in the front-end (React) and back-end (Node, Express, PostgreSQL, Mongo).",
+    " ▲ Ability to create clean, modern, testable, well-documented code.",
+    "▲ English fluency, verbal and written (C2 Proficient).",
+  ],
+
+  address: "Argentina",
+  website: "https://github.com/Baltaguirre",
+  education: [
+    {
+      UniversityName: "Henry",
+      specialization: "Full-Stack Developer",
+      MonthOfPassing: "Sep",
+      YearOfPassing: "2021",
+      Achievements: `Bootcamp de desarrollo full stack | 
+        
+        +50 hs de código por semana. 
+       
+        
+        - HTML 
+        - Css
+        - JavaScript
+        - React Js.
+        - Redux
+        - NodeJs
+        - Express
+        - Postgres
+        - Sequelize
+        - Postman`,
+    },
+    {
+      UniversityName: "Universidad Nacional de Córdoba",
+      specialization: "Lic. en Psicología",
+      MonthOfPassing: "Dic",
+      YearOfPassing: "2015",
+      Achievements:
+        "Primer y segundo año de la Licenciatura en Psicología completos",
+    },
+    {
+      UniversityName: "Colegio Universitario IES",
+      specialization: "Técnico en Turismo",
+      MonthOfPassing: "Mar",
+      YearOfPassing: "2011",
+      Achievements:
+        "Durante el primer año de la Tecnicatura en Turismo cursé materias relacionadas a la administración de organizaciones abocadas a lo turístico tales como: Organización Empresaria y Mercados Turísticos en el primer semestre y Recursos Naturales y Servicios Turísticos en el segundo, as{i como una Práctica Profesionalizante. En cuanto a idiomas cursé las materias de Inglés I y II y Portugués I y II, y además la materia Recursos Informáticos.",
+    },
+    {
+      UniversityName: "La Metro - Escuela de Cine y Televisión",
+      specialization: "Técnico en Cine y Televisión",
+      MonthOfPassing: "Dec",
+      YearOfPassing: "2008",
+      Achievements:
+        "Cursé los dos primeros años de la tecnicatura en Cine, Video y Televisión, donde asistí y completé clases de asignaturas tales como Dirección, Iluminación, Guión, Cámara, Producción, Sonido, Edición e Historia del Cine. Participé en la creación y puesta en escena de diversos trabajos prácticos así como de dos trabajos finales (cortometraje de ficción y programa televisivo) como asistente de dirección en el primero y como productor en el segundo.",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Luca",
+      specialization: "Full-Stack Developer",
+      MonthOfLeaving: "Actual",
+      YearOfLeaving: "",
+      Achievements:
+        "Actualmente me encuentro desempeñando el cargo de Full-Stack Developer en Luca, y como parte de mis responsabilidades, me encargo del mantenimiento y desarrollo de nuevos features para la plataforma, así como de optimizar y refactorizar el código existente con el objetivo de lograr una base de código sólida, escalable y limpia. En este rol, he tenido la oportunidad de aplicar mis habilidades técnicas para desarrollar soluciones creativas y eficientes que satisfagan las necesidades de nuestros usuarios. Además, he trabajado de cerca con mi equipo para asegurarme de que los proyectos se entreguen a tiempo y de que se cumplan los estándares de calidad. También he tenido la oportunidad de aplicar mis habilidades de resolución de problemas y pensamiento crítico para identificar y resolver problemas de manera rápida y efectiva, lo que me ha permitido contribuir de manera significativa al éxito de la plataforma. En resumen, trabajar en Luca ha sido una experiencia enriquecedora que me ha permitido seguir creciendo como desarrollador y aplicar mis habilidades en un ambiente desafiante y gratificante.",
+    },
+    {
+      CompanyName: "Reanswer",
+      specialization: "Full-Stack Developer",
+      MonthOfLeaving: "May",
+      YearOfLeaving: "2022",
+      Achievements:
+        "Durante mi experiencia en Reanswer, una software factory argentina, formé parte del equipo de desarrollo de una plataforma de fidelización de clientes. En este proyecto, trabajamos bajo la metodología ágil Scrum y aplicamos los principios de Clean Code y Clean Architecture para desarrollar soluciones eficientes y escalables. Para el front-end, utilicé React Native y React JS, mientras que para el back-end, trabajé con MoleculerJS y Node JS, en un ambiente de microservicios y MongoDB como base de datos. Fue una oportunidad única de aplicar mis habilidades técnicas y trabajar en equipo para lograr un proyecto exitoso y satisfacer las necesidades de los usuarios.",
+    },
+    {
+      CompanyName: "Digital Mottus",
+      specialization: "Trainee Front-End Developer",
+      MonthOfLeaving: "Nov",
+      YearOfLeaving: "2021",
+      Achievements:
+        "Durante mi primer experiencia como trainee en Digital Mottus, una startup con sede en Córdoba, tuve la oportunidad de trabajar en un entorno profesional. Colaboré en el desarrollo Front-End de una plataforma para la administración de stock de tiendas comerciales, formando parte de un proyecto emocionante que me permitió aplicar y mejorar mis habilidades técnicas.",
+    },
+  ],
+  skillsDescription: "",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "ReactJs",
+    },
+    {
+      skillname: "React-Native",
+    },
+    {
+      skillname: "NodeJs",
+    },
+    {
+      skillname: "PostgreSQL",
+    },
+    {
+      skillname: "Express",
+    },
+    {
+      skillname: "GraphQL",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+
+export default resumeData;
